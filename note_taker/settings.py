3c291b22ecd2d05efedcb2f1f6904545779792d5
Brandon White <bwhite06@stac.edu>
2019-02-06 06:32:57 -0500

    app is fully functioning on local

--- note_taker/settings.py
+++ note_taker/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'notes',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken'
+
 ]
 
 MIDDLEWARE = [
--- note_taker/urls.py
+++ note_taker/urls.py
@@ -5,11 +5,14 @@ from django.urls import path
 from rest_framework import routers
 from notes.api import PersonalNoteViewSet
 from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
 router.register(r'notes', PersonalNoteViewSet)
 
 urlpatterns = [
+    re_path(r'^api-token-auth/', views.obtain_auth_token),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
